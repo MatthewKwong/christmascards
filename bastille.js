9fe5c3940b4fb04674af0df571400fe00b7ecaaa
Matthew Kwong <mkwong.sh@mysummitps.org>
2016-12-13 07:28:23 +0000

    first commit

---
+++ bastille.js
@@ -0,0 +1,11 @@
+/*Each part of the triangle*/
+$("#1").hide(0).delay(100).fadeIn(750);
+$("#2").hide(0).delay(500).fadeIn(750);
+$("#3").hide(0).delay(700).fadeIn(750);
+$("#4").hide(0).delay(1000).fadeIn(750);
+/*Wild World Communications*/
+$("#wwcs").hide(0).delay(1300).fadeIn(750);
+/*Pit of Passion*/
+$("#pitOfPassion").hide(0).delay(1600).fadeIn(750);
+/*Christmas*/
+$("#merryChristmas").hide(0).delay(1900).fadeIn(750);
